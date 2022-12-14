91d32b30f556044843744fedab8513a92279d565
Hassan Balouch <hassan.balouch333@gmail.com>
2022-12-14 05:38:42 +0500

    shuffle

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default function App() {
+  let arr = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
+  const [cards, setCards] = useState(arr);
+  const shuffle = () => {
+    const shuffleArr = [...arr].sort(() => Math.random() - 0.5);
+    setCards(shuffleArr);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.wrapper}>
+        {cards.map((item, index) => {
+          return (
+            <View item={item} key={index} style={styles.box}>
+              <Text>{item}</Text>
+            </View>
+          );
+        })}
+      </View>
+      <TouchableOpacity style={styles.button} onPress={shuffle}>
+        <Text>Shuffle</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#222",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  wrapper: {
+    height: "50%",
+    width: "100%",
+    marginTop: 50,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  },
+  box: {
+    height: "18%",
+    width: "18%",
+    backgroundColor: "#e5e5e5",
+    margin: 20,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    height: "7%",
+    width: "60%",
+    borderRadius: 10,
+    backgroundColor: "yellow",
+    alignSelf: "center",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
